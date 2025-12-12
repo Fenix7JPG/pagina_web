60e8116a9bd68c238de1a14b5daaddb63eceddd1
Fenix7JPG <fenix7jpg@gmail.com>
2025-12-12 12:24:44 -0500

    Add functions to open and close cart menu

--- test/test3/script_menu_main.js
+++ test/test3/script_menu_main.js
@@ -16,3 +16,28 @@ document.querySelector(".menu_side_header").addEventListener("click", () => {
     document.getElementById("menu_side").classList.remove("show");
 });
 
+
+
+
+//Abrir menú carrito
+function openCartSide() {
+    console.log("Abriendo menú");
+    document.getElementById("cart_side").classList.add("show");
+}
+
+// Cerrar menú carrito
+document.querySelector(".cart_side_header").addEventListener("click", () => {
+    console.log("Cerrando menú");
+    document.getElementById("cart_side").classList.remove("show");
+});
+
+
+
+// Mostrar productos en el carrito
+function showProduct(idProducto) {
+
+  // Mostrar solo el producto deseado agregando la clase 'show'
+  const div = document.getElementById(idProducto);
+  div.classList.add("show");
+}
+
